d6f3a32da7e11722a3290fc9da3af1be4bb05e79
Suleka Dissanayake <sulekadissanayake2003@gmail.com>
2025-08-26 19:19:09 +0530

    Add files via upload

---
+++ DAY 02/Tutorial - Q1/Tutorial/Q1.cs
@@ -0,0 +1,27 @@
+﻿// Question 01
+using System;
+
+class Q1
+{
+    static void Main()
+    {
+        // Input two numbers
+        Console.Write("Enter the first number: ");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter the second number: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        // Perform operations
+        double sum = num1 + num2;
+        double difference = num1 - num2;
+        double product = num1 * num2;
+        double quotient =  num1 / num2
+
+        // Display results
+        Console.WriteLine($"\nSum: {sum}");
+        Console.WriteLine($"Difference: {difference}");
+        Console.WriteLine($"Multiplication: {product}");
+        Console.WriteLine($"Division: {quotient}");
+    }
+}
---
+++ DAY 02/Tutorial - Q2/Tutorial - Q2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        for (int i = 1; i < 11; i++)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
