e0e0b6001e0714913824181d6663c37f4147641d
Suleka Dissanayake <sulekadissanayake2003@gmail.com>
2025-08-26 19:24:54 +0530

    Add files via upload

---
+++ DAY 02/Tutorial - Q9/Tutorial - Q9/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Vehicle
+{
+    private int chassyNo;
+    private string name;
+
+    public Vehicle(int chassyNo, string name)
+    {
+        this.chassyNo = chassyNo;
+        this.name = name;
+    }
+
+    public void Drive()
+    {
+        Console.WriteLine("driving");
+    }
+}
+
+class Car : Vehicle
+{
+    int door;
+
+    public Car(int chassyNo, string name, int door) : base(chassyNo, name)
+    {
+        this.door = door;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Car myCar = new Car(12345, "Toyota", 4);
+        myCar.Drive();
+    }
+}
