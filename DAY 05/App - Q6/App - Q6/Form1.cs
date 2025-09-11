34ecffb057c65f056a5289effbc2c66cd2400aae
Suleka Dissanayake <sulekadissanayake2003@gmail.com>
2025-09-11 12:45:27 +0530

    Add files via upload

---
+++ DAY 05/App - Q6/App - Q6/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace App___Q6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(Convert.ToInt32(textBox1.Text) > 40)
+            {
+                label2.Text = "Pass";
+            }
+            else
+            {
+                label2.Text = "Fail";
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ DAY 05/App - Q7/App - Q7/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace App___Q7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(Convert.ToInt32(textBox1.Text) % 2 == 0)
+            {
+                label2.Text = "Even Number";
+            }
+            else
+            {
+                label2.Text = "Odd Number";
+            }
+        }
+    }
+}
