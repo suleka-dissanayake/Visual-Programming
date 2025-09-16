8c52033f2cb48d36fd625cb3c1fdaf27a7ac08ae
Suleka Dissanayake <sulekadissanayake2003@gmail.com>
2025-09-16 10:36:52 +0530

    Add files via upload

---
+++ DAY 07/Age Calculator/Age Calculator/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Age_Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label2.Visible = true;
+
+            try
+            {
+                DateTime dob = DateTime.Parse(textBox1.Text);
+                int age = DateTime.Now.Year - dob.Year;
+
+                if (DateTime.Now.DayOfYear < dob.DayOfYear)
+                    age--;
+
+            label2.Text = $"Your age: {age.ToString()} years";
+            }
+            catch
+            {
+                label2.Text = "Invalid!";
+            }
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            textBox1.Text = dateTimePicker1.Value.ToString();
+        }
+    }
+}
---
+++ DAY 07/Change BgColor/Change BgColor/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Change_BgColor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.MouseMove += new MouseEventHandler(Form1_Load);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            this.BackColor = Color.FromArgb(random.Next(256), random.Next(256), random.Next(256));
+        }
+    }
+}
