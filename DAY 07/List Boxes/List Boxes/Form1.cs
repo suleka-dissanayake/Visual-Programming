253240425be7637bd3b68422e804eb6a139dcd10
Suleka Dissanayake <sulekadissanayake2003@gmail.com>
2025-09-16 10:39:30 +0530

    Add files via upload

---
+++ DAY 07/List Boxes/List Boxes/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace List_Boxes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(listBox1.SelectedItem != null)
+            {
+                listBox2.Items.Add(listBox1.SelectedItem.ToString());
+            }
+            else
+            {
+                MessageBox.Show("Please select an item first!", "No item Selected", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void listBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ DAY 07/Progress Bar/Progress Bar/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Progress_Bar
+{
+    public partial class Form1 : Form
+    {
+        private Timer timer = new Timer();
+        public Form1()
+        {
+            InitializeComponent();
+            timer.Interval = 50;
+            timer.Tick += timer1_Tick;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            progressBar1.Value = 0;
+            timer1.Start();
+            progressBar1.Show();
+            button1.Hide();
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(progressBar1.Value < 100)
+            {
+                progressBar1.Value++;
+            }
+            else
+            {
+                timer1.Stop();
+                MessageBox.Show("Completed!");
+            }
+        }
+    }
+}
